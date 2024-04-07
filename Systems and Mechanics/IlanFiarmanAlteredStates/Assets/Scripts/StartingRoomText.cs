1f54f873433627eb0d354233882ebcda03d8fbbc
Ilan <ilan.fiarman@mymail.champlain.edu>
2024-04-06 18:45:23 -0700

    Altered States
    Beginning work

---
+++ Systems and Mechanics/IlanFiarmanAlteredStates/Assets/Scripts/StartingRoomText.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingRoomText : MonoBehaviour
+{
+    public GameObject textBox;
+    public GameObject line1;
+    public GameObject line2;
+    public GameObject line3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
